5fd21e1dd9a24cf6a08840fe35dd0479c4a6bce2
Habra <habragit5000@outlook.com>
2026-05-05 22:15:38 -0400

    Harden astronaut duties lookup query

--- api/Business/Queries/GetAstronautDutiesByName.cs
+++ api/Business/Queries/GetAstronautDutiesByName.cs
@@ -3,6 +3,7 @@ using MediatR;
 using StargateAPI.Business.Data;
 using StargateAPI.Business.Dtos;
 using StargateAPI.Controllers;
+using System.Net;
 
 namespace StargateAPI.Business.Queries
 {
@@ -22,18 +23,51 @@ namespace StargateAPI.Business.Queries
 
         public async Task<GetAstronautDutiesByNameResult> Handle(GetAstronautDutiesByName request, CancellationToken cancellationToken)
         {
-
             var result = new GetAstronautDutiesByNameResult();
 
-            var query = $"SELECT a.Id as PersonId, a.Name, b.CurrentRank, b.CurrentDutyTitle, b.CareerStartDate, b.CareerEndDate FROM [Person] a LEFT JOIN [AstronautDetail] b on b.PersonId = a.Id WHERE \'{request.Name}\' = a.Name";
+            var query = @"
+                SELECT
+                    a.Id as PersonId,
+                    a.Name,
+                    b.CurrentRank,
+                    b.CurrentDutyTitle,
+                    b.CareerStartDate,
+                    b.CareerEndDate
+                FROM [Person] a
+                LEFT JOIN [AstronautDetail] b on b.PersonId = a.Id
+                WHERE a.Name = @Name";
 
-            var person = await _context.Connection.QueryFirstOrDefaultAsync<PersonAstronaut>(query);
+            var person = await _context.Connection.QueryFirstOrDefaultAsync<PersonAstronaut>(
+                query,
+                new { Name = request.Name }
+            );
+
+            if (person is null)
+            {
+                result.Success = false;
+                result.Message = "Person not found";
+                result.ResponseCode = (int)HttpStatusCode.NotFound;
+                return result;
+            }
 
             result.Person = person;
 
-            query = $"SELECT * FROM [AstronautDuty] WHERE {person.PersonId} = PersonId Order By DutyStartDate Desc";
+            query = @"
+                SELECT
+                    Id,
+                    PersonId,
+                    Rank,
+                    DutyTitle,
+                    DutyStartDate,
+                    DutyEndDate
+                FROM [AstronautDuty]
+                WHERE PersonId = @PersonId
+                Order By DutyStartDate Desc";
 
-            var duties = await _context.Connection.QueryAsync<AstronautDuty>(query);
+            var duties = await _context.Connection.QueryAsync<AstronautDuty>(
+                query,
+                new { PersonId = person.PersonId }
+            );
 
             result.AstronautDuties = duties.ToList();
 
@@ -44,7 +78,7 @@ namespace StargateAPI.Business.Queries
 
     public class GetAstronautDutiesByNameResult : BaseResponse
     {
-        public PersonAstronaut Person { get; set; }
+        public PersonAstronaut? Person { get; set; }
         public List<AstronautDuty> AstronautDuties { get; set; } = new List<AstronautDuty>();
     }
 }
